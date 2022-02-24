b47c91176b8a203431bf408e72816c8c3adf5168
pervin0527 <zfbtldnz@gmail.com>
2022-02-24 09:15:08 +0900

    updated 2022.02.24

--- deep_learning_base/functions.py
+++ deep_learning_base/functions.py
@@ -29,6 +29,26 @@ def cross_entropy_error(y, t):
     batch_size = y.shape[0]
     return -np.sum(t * np.log(y + 1e-7)) / batch_size
 
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+#     print(x.shape, grad.shape)
+    
+#     print(x.size)
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+        
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+        
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = tmp_val
+        
+    return grad
+
 if __name__ == "__main__":
     test2 = np.array([0.3, 2.9, 4.0])
     print(softmax(test2))
