6b858b69b50748443368b5898a491b1e3c402dcb
pervin0527 <zfbtldnz@gmail.com>
2022-03-04 11:07:39 +0900

    updated 2022.03.04

--- deep_learning_base/functions.py
+++ deep_learning_base/functions.py
@@ -1,51 +1,60 @@
 import numpy as np
 
+
+def identity_function(x):
+    return x
+
+
 def step_function(x):
-    y = x > 0
-    return y.astype(np.int64)
+    return np.array(x > 0, dtype=np.int)
+
 
 def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+    return 1 / (1 + np.exp(-x))    
 
-def ReLU(x):
+
+def sigmoid_grad(x):
+    return (1.0 - sigmoid(x)) * sigmoid(x)
+    
+
+def relu(x):
     return np.maximum(0, x)
 
-def softmax(x):
-    c = np.max(x)
-    exp_x = np.exp(x - c)
-    sum_exp_x = np.sum(exp_x)
-    y = exp_x / sum_exp_x
-    
-    return y
 
-def identity_func(x):
-    return x
+def relu_grad(x):
+    grad = np.zeros(x)
+    grad[x>=0] = 1
+    return grad
+    
+
+def softmax(x):
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T 
+
+    x = x - np.max(x) # 오버플로 대책
+    return np.exp(x) / np.sum(np.exp(x))
+
+
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y-t)**2)
+
 
 def cross_entropy_error(y, t):
     if y.ndim == 1:
         t = t.reshape(1, t.size)
         y = y.reshape(1, y.size)
         
+    # 훈련 데이터가 원-핫 벡터라면 정답 레이블의 인덱스로 반환
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+             
     batch_size = y.shape[0]
-    return -np.sum(t * np.log(y + 1e-7)) / batch_size
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
 
-def numerical_gradient(f, x):
-    h = 1e-4
-    grad = np.zeros_like(x)
-    for idx in range(x.size):
-        tmp_val = x[idx]
-        
-        x[idx] = tmp_val + h
-        fxh1 = f(x)
-        
-        x[idx] = tmp_val - h
-        fxh2 = f(x)
-        
-        grad[idx] = (fxh1 - fxh2) / (2 * h)
-        x[idx] = tmp_val
-        
-    return grad
 
-if __name__ == "__main__":
-    test2 = np.array([0.3, 2.9, 4.0])
-    print(softmax(test2))
+def softmax_loss(X, t):
+    y = softmax(X)
+    return cross_entropy_error(y, t)
